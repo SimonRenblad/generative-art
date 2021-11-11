5081f06bd3830e3a3f4340ba0b6ad4567e09bb89
Simon Renblad <simon.renblad@outlook.com>
2021-11-11 22:53:26 +0800

    created curved lisa

--- script.py
+++ script.py
@@ -4,14 +4,15 @@ import matplotlib.pyplot as plt
 from samila import GenerativeImage
 
 def f1(x,y):
-    result = y * math.cos(5*x)
+    result = random.uniform(-1,1) * x**2  - math.sin(y**2) + abs(y-x)
     return result
 
 def f2(x,y):
-    result = x * math.sin(5*y)
+    result = random.uniform(-1,1) * y**3 - math.cos(x**2) + 2*x
     return result
 
 g = GenerativeImage(f1,f2)
 g.generate()
 g.plot()
-plt.savefig('test.png')
+print(g.seed)
+plt.savefig('test3-'+str(g.seed)+'.png', transparent=True)
